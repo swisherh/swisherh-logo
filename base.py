9f8f1c80d670ab73efce49308fc879bfb6d0bbe5
Harald Schilly <harald.schilly@gmail.com>
2011-08-24 17:07:49 +0200

    tuning how links are displayed

---
+++ base.py
@@ -0,0 +1,62 @@
+# -*- encoding: utf-8 -*-
+
+import sys
+import logging
+from flask import Flask, session, g, render_template, url_for, request, redirect
+from pymongo import Connection
+from sage.all import *
+from functools import wraps
+from werkzeug.contrib.cache import SimpleCache
+
+_C = None
+ 
+def _init(dbport):
+  global _C
+  if not _C:
+    logging.info("establishing db connection at port %s" % dbport)
+    _C = Connection(port=dbport, network_timeout=1)
+
+def getDBConnection():
+  return _C
+
+app = Flask(__name__)
+
+# the following context processor inserts
+#  * empty info={} dict variable
+#  * body_class = ''
+#  * bread = [...] for the default bread crumb hierarch
+#  * title = 'test string'
+@app.context_processor
+def ctx_proc_userdata():
+  # insert an empty info={} as default
+  # set the body class to some default, blueprints should
+  # overwrite it with their name, using @<blueprint_object>.context_processor
+  # see http://flask.pocoo.org/docs/api/?highlight=context_processor#flask.Blueprint.context_processor
+  vars = { 'info' : {}, 'body_class' : '' }
+
+  # insert the default bread crumb hierarchy
+  # overwrite this variable when you want to customize it
+  vars['bread'] = [ ('Bread', '.'), ('Crumb', '.'), ('Hierarchy', '.')]
+  
+  # default title
+  vars['title'] = r'Title variable "title" has not been set. This is a test: \( \LaTeX \) and \( \frac{1}{1+x+x^2} \) and more ...'
+  return vars
+
+
+# datetime format in jinja templates
+# you can now pass in a datetime.datetime python object and via
+# {{ <datetimeobject>|fmtdatetime }} you can format it right inside the template
+# if you want to do more than just the default, use it for example this way:
+# {{ <datetimeobject>|fmtdatetime('%H:%M:%S') }}
+@app.template_filter("fmtdatetime")
+def fmtdatetime(value, format='%Y-%m-%d %H:%M:%S'):
+    return value.strftime(format)
+
+@app.template_filter('obfuscate_email')
+def obfuscate_email(email):
+    """
+    obfuscating the email
+    TODO: doesn't work yet
+    """
+    return u"%s…@…%s" % (email[:2],email[-2:])
+
---
+++ upload/upload.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+# This Blueprint is for handling data uploads.
+# Authenticated users also have access to the raw entries, 
+# selected users can edit metadata, …
+#
+# author: 
+
+import logging
+import pymongo
+import flask
+import datetime
+import json
+from base import app, getDBConnection, fmtdatetime
+from flask import render_template, request, abort, Blueprint, url_for
+from flaskext.login import login_required, current_user
+from gridfs import GridFS
+from pymongo.objectid import ObjectId
+
+from users import admin_required
+
+
+upload_page = Blueprint("upload", __name__, template_folder='templates')
+
+# blueprint specific definition of the body_class variable
+@upload_page.context_processor
+def body_class():
+  return { 'body_class' : 'upload' }
+
+def get_bread():
+  return [("Upload", url_for(".index")) ]
+
+@upload_page.route("/")
+@login_required
+def index():
+  return render_template("upload-index.html", title = "Data Upload", bread = get_bread())
+
+@upload_page.route("/upload", methods = ["POST"])
+@login_required
+def upload():
+  metadata = {
+    "name": request.form['name'],
+    "full_description": request.form['full_description'],
+    "data_format": request.form['data_format'],
+    "creator": request.form['creator'],
+    "reference": request.form['reference'],
+    "bibtex": request.form['bibtex'],
+    "uploader": current_user.name,
+    "time": datetime.datetime.utcnow(),
+    "original_file_name": request.files['file'].filename,
+    "status": "unmoderated",
+    "version": "1"
+  }
+  flask.flash("Received file: " + request.files['file'].filename)
+
+  upload_db = getDBConnection().upload
+  upload_fs = GridFS(upload_db)
+  upload_fs.put(request.files['file'].read(), metadata = metadata, filename=request.files['file'].filename)
+  
+  return flask.redirect(url_for(".index"))
+
+@upload_page.route("/admin", methods = ["POST"])
+@admin_required
+def admin_update():
+
+  db = getDBConnection().upload
+  id = request.form['id']
+
+  if request.form.has_key('approve'):
+    db.fs.files.update({"_id" : ObjectId(id)}, {"$set": {"metadata.status" : "approved"}})
+    flask.flash('Approved')
+  if request.form.has_key('disapprove'):
+    db.fs.files.update({"_id" : ObjectId(id)}, {"$set": {"metadata.status" : "disapproved"}})
+    flask.flash('Disapproved')
+
+  return flask.redirect(url_for(".admin"))
+
+
+@upload_page.route("/admin", methods = ["GET"])
+@admin_required
+def admin():
+
+  db = getDBConnection().upload
+  fs = GridFS(db)
+
+  unmoderated = [ fs.get(x['_id']) for x in db.fs.files.find({"metadata.status" : "unmoderated"}) ]
+  approved = [ fs.get(x['_id']) for x in db.fs.files.find({"metadata.status" : "approved"}) ]
+  disapproved = [ fs.get(x['_id']) for x in db.fs.files.find({"metadata.status" : "disapproved"}) ]
+
+  return render_template("upload-admin.html", title = "Data Upload", bread = get_bread(), unmoderated=unmoderated, approved=approved, disapproved=disapproved)
